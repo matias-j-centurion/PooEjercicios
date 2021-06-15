9f7e76d5962f153ba8fd778bb8e0c5bdbac02661
PapaMatu2302 <matiasjoelcenturion@gmail.com>
2021-06-14 21:40:12 -0300

    Ejercicios POO

---
+++ Poo.ConsoleApp/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Poo.Models;
+
+
+namespace Poo.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Libro l1 = new Libro();
+            // Enciclopedia e1 = new Enciclopedia();
+            // Console.WriteLine("Ingrese 1 si quiere ver la informacion de una enciclopedia, 2 si quiere prestar/devolver un libro");
+            // int opcion = Int32.Parse(Console.ReadLine());
+            // if(opcion == 1)
+            // {
+            //     Console.WriteLine("Introducir el titulo de la Enciclopedia");
+            //     e1.titulo = Console.ReadLine();
+            //     Console.WriteLine("Introducir autor de la enciclopedia");
+            //     e1.Autor = Console.ReadLine();
+            //     Console.WriteLine("Introducir cantidad total de tomos de la enciclopedia");
+            //     e1.tomos = int.Parse(Console.ReadLine());
+            //     Console.WriteLine(e1.getInfoLibro());
+            // }else if (opcion == 2)
+            // {
+            Console.WriteLine("Introducir titulo del libro");
+            l1.Titulo = Console.ReadLine();
+            Console.WriteLine("Introducir autor del libro");
+            l1.Autor = Console.ReadLine();
+            Console.WriteLine("Introducir cantidad total de copias del libro");
+            l1.Copias = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introducir numero de unidades prestadas del libro");
+            l1.Prestados = int.Parse(Console.ReadLine());
+            Console.WriteLine(l1.getInfoLibro());
+            // }
+            Console.WriteLine("Si se pudo prestar o devolver el libro, aparecerá 'true', caso contrario, se leerá 'false'");
+
+            Console.WriteLine("Si desea prestar un libro, presione 1. Si desea devolver un libro, presione 2");
+            
+            int opcion = Int32.Parse(Console.ReadLine());
+            if (opcion == 1)
+            {
+                if (l1.Prestar())
+                {
+                
+                    Console.WriteLine($"Se ha prestado el libro {l1.titulo}");
+                    
+                }
+                else
+                {
+                    Console.WriteLine($"No quedan copias de {l1.titulo} para prestar");
+                }
+            }
+            else if (opcion == 2)
+            {
+                
+                if (l1.Devolver())
+                {
+                   
+                   Console.WriteLine($"Se ha devuelvo el libro {l1.titulo}");
+                }
+                else
+                {
+                    Console.WriteLine($"No se puede devolver {l1.titulo} ya que no hay unidades que devolver");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Opción Incorrecta");
+            }
+            
+
+        }
+
+    }
+}
---
+++ Poo.Models/Models.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Poo.Models
+{
+    public class Libro
+    {
+        public string titulo;
+        public string Titulo
+        {
+            set { this.titulo = value; }
+            get { return this.titulo; }
+        }
+        public string autor;
+        public string Autor
+        {
+            set { this.autor = value; }
+            get { return this.autor; }
+        }
+        public int copias;
+        public int Copias
+        {
+            set { this.copias = value; }
+            get { return this.copias; }
+        }
+        public int prestados;
+        public int Prestados
+        {
+            set { this.prestados = value; }
+            get { return this.prestados; }
+        }
+        public Libro()
+        {
+            //constructor por defecto
+        }
+
+        public Libro(string titulo, string autor, int copias, int prestados)
+        {
+            //Constructor
+            this.titulo = titulo;
+            this.autor = autor;
+            this.copias = copias;
+            this.prestados = prestados;
+        }
+
+
+        //Metodos
+
+        public bool Prestar()
+        {
+            bool prest = true;
+            if (prestados < copias)
+            {
+                prestados++;
+            }
+            else
+            {
+                prest = false;
+            }
+            Console.WriteLine(prest);
+            return prest;
+            ;
+
+        }
+
+
+        public bool Devolver()
+        {
+
+            bool dev = true;
+            if (prestados == 0)
+            {
+                dev = false;
+            }
+            else
+            {
+                prestados--;
+            }
+            Console.WriteLine(dev);
+            return dev;
+
+        }
+         public string getInfoLibro()
+        {
+            return $"El nombre del libro es {Titulo}, su autor es {Autor}. La cantidad de copias es {Copias} y la cantidad de copias prestadas es {Prestados}";
+        }
+        
+    }
+    
+    
+}
